848f72170ed9ad4a8459b737334df284b02b4671
hehoku <hehoku@outlook.com>
2023-06-03 23:17:47 +0800

    feat: add nav and sort for tailwindcss class name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import Nav from "@/components/nav/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Nav />
     </main>
-  )
+  );
 }
---
+++ src/components/nav/page.tsx
@@ -0,0 +1,7 @@
+export default function Nav() {
+  return (
+    <div className="flex w-full flex-row items-start justify-start border-b-[1px] border-black p-4">
+      <p className="cursor-pointer text-4xl font-extrabold">MathNoMyth</p>
+    </div>
+  );
+}
