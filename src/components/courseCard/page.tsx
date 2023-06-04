12276b77e73f5fd220a445406a41582d6130e23c
hehoku <hehoku@outlook.com>
2023-06-04 23:00:49 +0800

    ui: change background to white

--- src/components/courseCard/page.tsx
+++ src/components/courseCard/page.tsx
@@ -1,6 +1,6 @@
 export default function CourseCard() {
   return (
-    <div className="flex cursor-pointer flex-col items-start gap-1 rounded-md px-6 py-4 hover:bg-gray-200">
+    <div className="flex cursor-pointer flex-col items-start gap-1 rounded-md px-6 py-4 hover:bg-gray-100">
       <div className="h-60 w-80 rounded-md bg-gray-400"></div>
       <p className="text-gray-500">高一数学</p>
       <p className="text-2xl font-bold">三角函数的周期变化</p>
