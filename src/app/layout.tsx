d2159104843a6bf8e055b129ef4db004e5a7337c
hehoku <hehoku@outlook.com>
2023-06-05 22:02:22 +0800

    feat: refactor page: nav and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,28 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Nav from "@/components/nav/page";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'MathNoMyth',
-  description: 'A little assistant to help you with your mathematics studies.',
-}
+  title: "MathNoMyth",
+  description: "A little assistant to help you with your mathematics studies.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-      <footer className="p-4 text-center border-t-[1px] text-gray-400">©2023 IN2 Studio</footer>
+      <body className={inter.className}>
+        <Nav />
+        {children}
+      </body>
+      <div className="border-t-[1px] p-4 text-center text-gray-400">
+        ©2023 IN2 Studio
+      </div>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import CourseCard from "@/components/courseCard/page";
-import Nav from "@/components/nav/page";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center pb-10">
-      <Nav />
       <div className="mt-10 flex w-5/6 flex-row flex-wrap items-start justify-start gap-8">
-        <CourseCard />
+        <Link href="periodic-101">
+          <CourseCard />
+        </Link>
         <CourseCard />
         <CourseCard />
         <CourseCard />
