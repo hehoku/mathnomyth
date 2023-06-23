dd01bf07064c40673facce9ad88cb3d6e9c1e8ea
hehoku <hehoku@outlook.com>
2023-06-23 23:14:28 +0800

    ui: adjust layout of course page

--- src/app/periodic-101/page.tsx
+++ src/app/periodic-101/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <>
-      <p>Test</p>
-    </>
-  )
+    <div className="flex w-3/4 flex-col gap-4 p-10">
+      <h2 className="text-2xl font-bold">三角函数</h2>
+      <h3 className="text-xl">弧度</h3>
+    </div>
+  );
 }
